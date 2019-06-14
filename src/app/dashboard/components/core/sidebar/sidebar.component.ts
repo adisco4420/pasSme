5fb51981d1aaebeca3963a52cb8b8be1c5a75da2
Alabi Sodiq <adisco4420@gmail.com>
2019-06-14 22:47:42 +0100

    update sidebar

--- src/app/dashboard/components/core/sidebar/sidebar.component.ts
+++ src/app/dashboard/components/core/sidebar/sidebar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var $;
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
@@ -10,6 +10,17 @@ export class SidebarComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.loadScript();
+  }
+  loadScript() {
+    $("#sidebar").mCustomScrollbar({
+      theme: "minimal"
+  });
+  $('#sidebarCollapse').on('click', function () {
+      $('#sidebar, #content').toggleClass('active');
+      $('.collapse.in').toggleClass('in');
+      $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+  });
   }
 
 }
