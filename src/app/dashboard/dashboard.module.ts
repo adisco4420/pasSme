31c434c445fc03b217004339297c5d2a1beac274
Alabi Sodiq <adisco4420@gmail.com>
2019-06-14 21:46:09 +0100

    generate header for dashboard

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -4,12 +4,13 @@ import { CommonModule } from '@angular/common';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { LandingComponent } from './components/landing/landing.component';
 import { SidebarComponent } from './components/core/sidebar/sidebar.component';
+import { DashHeaderComponent } from './components/core/dash-header/dash-header.component';
 
 @NgModule({
   imports: [
     CommonModule,
     DashboardRoutingModule
   ],
-  declarations: [LandingComponent, SidebarComponent]
+  declarations: [LandingComponent, SidebarComponent, DashHeaderComponent]
 })
 export class DashboardModule { }
