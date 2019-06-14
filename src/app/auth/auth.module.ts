18ebd9f889976dbe5155a2ce86c90188e26f2d31
Alabi Sodiq <adisco4420@gmail.com>
2019-06-14 10:43:25 +0100

    login and regsiter validation

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './components/login/login.component';
@@ -8,6 +9,8 @@ import { RegisterComponent } from './components/register/register.component';
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
     AuthRoutingModule
   ],
   declarations: [LoginComponent, RegisterComponent]
--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
-
+  registerForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email ]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+  });
   constructor() { }
+  get email() { return this.registerForm.get('email'); }
+  get password() { return this.registerForm.get('password'); }
 
   ngOnInit() {
+  }
+  register() {
+    console.log(this.registerForm.value);
+
   }
 
 }
