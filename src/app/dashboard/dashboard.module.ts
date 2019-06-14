5d4c1e47c656ac8c3836ef2bf1844f40a9300812
Alabi Sodiq <adisco4420@gmail.com>
2019-06-14 21:45:10 +0100

    generate sidebar for dashboard

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { LandingComponent } from './components/landing/landing.component';
+import { SidebarComponent } from './components/core/sidebar/sidebar.component';
 
 @NgModule({
   imports: [
     CommonModule,
     DashboardRoutingModule
   ],
-  declarations: [LandingComponent]
+  declarations: [LandingComponent, SidebarComponent]
 })
 export class DashboardModule { }
