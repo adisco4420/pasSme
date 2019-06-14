7ce073400a4d0ba2b3257dec7ebbe4378f9f2566
Alabi Sodiq <adisco4420@gmail.com>
2019-06-14 21:26:29 +0100

    validation in onboarding

--- src/app/auth/components/onboarding/onboarding.component.ts
+++ src/app/auth/components/onboarding/onboarding.component.ts
@@ -7,17 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./onboarding.component.scss']
 })
 export class OnboardingComponent implements OnInit {
+  submitted = false;
   onboardingForm = new FormGroup({
     businessName: new FormControl('', Validators.required),
     businessType: new FormControl('', Validators.required),
     businessCountry: new FormControl('', Validators.required),
     businessCurrency: new FormControl('', Validators.required),
   });
+  get onboardControl() {
+    return this.onboardingForm.controls;
+  }
+
   constructor() { }
 
   ngOnInit() {
   }
   setup() {
+    this.submitted = true;
     console.log(this.onboardingForm.value);
   }
 
