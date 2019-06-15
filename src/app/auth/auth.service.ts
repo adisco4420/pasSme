5d6d41a90ac85ac0ea5be3b61b688fbbd9a4ccac
Alabi Sodiq <adisco4420@gmail.com>
2019-06-15 11:24:15 +0100

    register user method in auth service

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,9 +1,16 @@
+import { environment } from './../../environments/environment.prod';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class AuthService {
+  private authUrl = `${environment.baseUrl}/users`;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  registerUser(user) {
+    return this.http.post(`${this.authUrl}/register/`, user);
+  }
 
 
 }
