35cae5e34c2d8e8c97504189447a62dddf0cd70d
Alabi Sodiq <adisco4420@gmail.com>
2019-06-15 12:34:44 +0100

    dec;lare register, login and is login method

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,6 @@
-import { environment } from './../../environments/environment.prod';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Injectable()
 export class AuthService {
@@ -9,8 +9,17 @@ export class AuthService {
   constructor(private http: HttpClient) { }
 
   registerUser(user) {
-    return this.http.post(`${this.authUrl}/register/`, user);
+    return this.http.post(`${this.authUrl}/register/`, user,
+    {headers: {'Content-Type': 'application/json'}});
   }
-
+  loginUser(user) {
+    return this.http.post(`${this.authUrl}/login/`, user);
+  }
+  storeToken(token: string) {
+    return localStorage.setItem('currentUser', token);
+  }
+  isLogin() {
+    return localStorage.getItem('currentUser');
+  }
 
 }
