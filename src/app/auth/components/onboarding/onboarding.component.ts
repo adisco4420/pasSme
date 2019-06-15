af86170ab8c81d5a344c71b22bff670565ef1610
Alabi Sodiq <adisco4420@gmail.com>
2019-06-15 20:37:07 +0100

    get currency and sector

--- src/app/auth/components/onboarding/onboarding.component.ts
+++ src/app/auth/components/onboarding/onboarding.component.ts
@@ -1,6 +1,9 @@
+import { CoreService } from './../../../core/core.service';
+import { AuthService } from './../../auth.service';
+import { Router } from '@angular/router';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
-
+import 'rxjs/add/operator/take';
 @Component({
   selector: 'app-onboarding',
   templateUrl: './onboarding.component.html',
@@ -11,6 +14,7 @@ export class OnboardingComponent implements OnInit {
   onboardingForm = new FormGroup({
     businessName: new FormControl('', Validators.required),
     businessType: new FormControl('', Validators.required),
+    businnessSector: new FormControl('', Validators.required),
     businessCountry: new FormControl('', Validators.required),
     businessCurrency: new FormControl('', Validators.required),
   });
@@ -18,13 +22,31 @@ export class OnboardingComponent implements OnInit {
     return this.onboardingForm.controls;
   }
 
-  constructor() { }
-
+  constructor(private router: Router, private coreSrv: CoreService) { }
+  countries = []; sectors = [];
+  currencies = [
+    {name: 'Dollar', slug: 'dollar'},
+    {name: 'Naira', slug: 'naira'},
+    {name: 'Euro', slug: 'euro'},
+    {name: 'Pounds', slug: 'pounds'}
+  ];
+  businessTypes = [
+    {name: 'Sole Proprietorship', slug: 'sole_proprietorship'},
+    {name: 'Partnership', slug: 'partnership'},
+    {name: 'Limited Liability', slug: 'limited_liability'},
+    {name: 'Coperative', slug: 'coperative'},
+    {name: 'Corporation', slug: 'corporation'}
+  ];
   ngOnInit() {
+    this.fetchData();
   }
   setup() {
     this.submitted = true;
     console.log(this.onboardingForm.value);
+  }
+  fetchData() {
+    this.coreSrv.getCountries().take(1).subscribe((data: any) => this.countries = data);
+    this.coreSrv.getSector().take(1).subscribe((data: any)  => this.sectors = data);
   }
 
 }
