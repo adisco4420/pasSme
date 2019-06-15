aa0354cbab8f172e5090d2e555241a0557d1a9bf
Alabi Sodiq <adisco4420@gmail.com>
2019-06-15 11:17:17 +0100

    import httpclientmodule in auth module

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -15,7 +16,8 @@ import { AuthService } from './auth.service';
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    HttpClientModule
   ],
   declarations: [LoginComponent, RegisterComponent, OnboardingComponent, ConfirmationComponent, ForgetPasswordComponent],
   providers: [AuthService]
