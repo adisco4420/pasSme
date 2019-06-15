1bbf1e3c7c50027dcbef315d05983d0774cf514b
Alabi Sodiq <adisco4420@gmail.com>
2019-06-15 20:37:25 +0100

    update register

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { AuthService } from './../../auth.service';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
@@ -8,12 +9,15 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
-  submitted = false
+  submitted = false;
+  loading = false;
+  errMsg = {status: false, code: 0 };
+  sucMsg = false;
   registerForm = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email ]),
     password: new FormControl('', [Validators.required, Validators.minLength(6)]),
   });
-  constructor(private authSrv: AuthService) { }
+  constructor(private authSrv: AuthService, private router: Router) { }
   get email() { return this.registerForm.get('email'); }
   get password() { return this.registerForm.get('password'); }
 
@@ -21,15 +25,25 @@ export class RegisterComponent implements OnInit {
   }
   register() {
     this.submitted = true;
-    console.log(this.registerForm.value);
     if (this.registerForm.valid) {
+      this.loading = true;
+      this.errMsg = {status: false, code: 0};
       this.authSrv.registerUser(this.registerForm.value).subscribe(data => {
-        console.log(data);
-      }, err => console.log(err));
+        this.loading = false; this.sucMsg = true;
+        this.goToSetup();
+      }, err => {
+        this.errMsg = {status: true, code: err.status};
+        this.loading = false;
+      });
     } else {
       console.log('invalid');
     }
 
+  }
+  goToSetup() {
+    setTimeout(() => {
+      this.router.navigate(['/auth/onboarding']);
+    }, 2000);
   }
 
 }
