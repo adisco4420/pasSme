2e69134b4c8a8faff19d673f079077afe02c9378
Mark <mark.oghifo@gmail.com>
2019-06-21 09:20:31 +0100

    shared module

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,13 @@
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  declarations: []
+})
+export class SharedModule { }
